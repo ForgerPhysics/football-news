948d696266c6c973d0d4b70c36ba875fb7b6815d
ForgerPhysics <sihalohoryan2@gmail.com>
2025-09-03 14:52:22 +0700

    selesai tutorial 1

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
+        'npm' : '2406419833',
         'name': 'Haru Urara',
         'class': 'PBP A'
     }
