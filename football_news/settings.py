da100b688454dd12e6f42c1fcaccab356cd9b522
ForgerPhysics <sihalohoryan2@gmail.com>
2025-09-10 14:53:07 +0700

    errornya semoga dah bener

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ryan-gibran-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ryan-gibran-footballnews.pbp.cs.ui.ac.id/"
+    "https://ryan-gibran-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
